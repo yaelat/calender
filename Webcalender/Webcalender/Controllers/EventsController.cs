17c1f5aa57401c61ee8a5fd5a1e6df5045617bfe
yaelat <yael240704@gmail.com>
2023-10-16 22:36:27 +0300

    the first status

---
+++ Webcalender/Webcalender/Controllers/EventsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace  Webcalender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private static List<Event> events=new List<Event> { new Event {Id=1,Title="default eventss",Start=DateTime.Now}, new Event { Id = 2, Title = "tom" , Start = DateTime.Now } };
+        private static int Id=3;
+        
+        // GET: api/<EventsController>
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return events;
+        }
+        // POST api/<EventsController>
+        [HttpPost]
+        public Event Post([FromBody] Event eve)
+        {
+            eve.Id = Id++;
+            events.Add(eve);
+            return eve;
+        }
+
+        // PUT api/<EventsController>/5
+        [HttpPut("{id}")]
+        public Event Put(int id, [FromBody] Event eve)
+        {
+            var nevent = events.Find(ev => ev.Id == id);
+            nevent.Start=eve.Start;
+            nevent.Title=eve.Title;
+            return nevent;
+
+        }
+
+        // DELETE api/<EventsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var nevent = events.Find(ev => ev.Id == id);
+            events.Remove(nevent);
+
+        }
+
+
+    }
+}
---
+++ Webcalender/Webcalender/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace Webcalender
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
